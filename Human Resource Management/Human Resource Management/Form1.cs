966bf151267367cbe414ace4735de003028ea302
Kohei-Suzukis <44814493+Kohei-Suzukis@users.noreply.github.com>
2020-11-13 03:23:30 +0000

    Starting to implement the file action functions. this is starting from as simple as possible for flexibility

--- Human Resource Management/Human Resource Management/Form1.cs
+++ Human Resource Management/Human Resource Management/Form1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+
 
 namespace Human_Resource_Management
 {
@@ -15,7 +20,48 @@ namespace Human_Resource_Management
         public Form1()
         {
             InitializeComponent();
+
+            var filename = Directory.GetCurrentDirectory() + @"\file.csv";
+            List<Person> list = new List<Person>() {
+                new  Person(){Name= "Ronaldo", Age= 29},
+                new  Person(){Name= "Missi", Age= 28}
+        };
+
+            WriteFile(filename, list);
+
+            ReadFile(filename);
+            Console.ReadLine();
+
+
         }
 
+        static void ReadFile(string filename)
+        {
+            TextReader textReader = File.OpenText(filename);
+            var csv = new CsvReader(textReader);
+
+            var people = csv.GetRecords<Person>();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Name {person.Name} age : {person.Age}");
+            }
+            textReader.Close();
+
+        }
+
+
+        static void WriteFile(string filename, IEnumerable<Person> people)
+        {
+                TextWriter textWriter = File.CreateText(filename);
+
+                var csvWriter = new CsvWriter(textWriter);
+                csvWriter.WriteRecords(people);
+
+                textWriter.Close();
+
+        }
+
+        
     }
 }
---
+++ Human Resource Management/Human Resource Management/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Human_Resource_Management
+{
+    class Person
+    {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+    }
+}
