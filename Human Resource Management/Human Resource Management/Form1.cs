472c4dd23ba31af79e15943d42c48c3e29b4e5e9
Kohei-Suzukis <44814493+Kohei-Suzukis@users.noreply.github.com>
2021-01-31 01:10:01 +0000

    cgange the read, write function method (add the file dialog to make it flexible). add the datagridview to view the data loaded.

--- Human Resource Management/Human Resource Management/Form1.cs
+++ Human Resource Management/Human Resource Management/Form1.cs
@@ -3,65 +3,185 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Text;
-using CsvHelper;
-using CsvHelper.Configuration.Attributes;
-
+using System.IO;
 
 namespace Human_Resource_Management
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
 
-            var filename = Directory.GetCurrentDirectory() + @"\file.csv";
-            List<Person> list = new List<Person>() {
-                new  Person(){Name= "Ronaldo", Age= 29},
-                new  Person(){Name= "Missi", Age= 28}
-        };
-
-            WriteFile(filename, list);
-
-            ReadFile(filename);
-            Console.ReadLine();
-
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
 
-        static void ReadFile(string filename)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            TextReader textReader = File.OpenText(filename);
-            var csv = new CsvReader(textReader);
 
-            var people = csv.GetRecords<Person>();
+        }
 
-            foreach (var person in people)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.Rows.Clear();
+            this.DataLoad();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            this.ExportFile();
+        }
+
+
+
+        /// <summary>
+        /// /////////////////////////////////
+        /// </summary>
+        /// 
+        string FileName = null;
+
+        public void DataLoad()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "OPEN FILE";
+            op.InitialDirectory = @"C:\";
+            op.FileName = null;
+            op.Filter = "CSV file(*.csv)|*.csv";
+            op.FilterIndex = 1;
+
+
+            DialogResult result = op.ShowDialog();
+
+            if (result == DialogResult.OK)
             {
-                Console.WriteLine($"Name {person.Name} age : {person.Age}");
+                string fileName = op.FileName;
+
+                string[] csvDataAll = File.ReadAllLines(op.FileName, Encoding.Default);
+
+                DataGridViewRow[] rows = new DataGridViewRow[csvDataAll.Length];
+
+                for (int i = 0; i < csvDataAll.Length; i++)
+                {
+                    string[] csv = csvDataAll[i].Split(',');
+                    string[] data = new string[9];
+                    Array.Copy(csv, 0, data, 0, 3);
+
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.CreateCells(this.dataGridView1);
+                    row.SetValues(data);
+                    rows[i] = row;
+                }
+
+                this.dataGridView1.Rows.AddRange(rows);
+
+                FileName = op.FileName;
             }
-            textReader.Close();
-
+            else if (result == DialogResult.Cancel)
+            {
+            }     
         }
-
-
-        static void WriteFile(string filename, IEnumerable<Person> people)
-        {
-                TextWriter textWriter = File.CreateText(filename);
-
-                var csvWriter = new CsvWriter(textWriter);
-                csvWriter.WriteRecords(people);
-
-                textWriter.Close();
-
-        }
-
         
+
+        public void SaveFile()
+        {
+            if (FileName != null)
+            {
+                using (StreamWriter writer = new StreamWriter(FileName))
+                {
+
+                    int rowCount = dataGridView1.Rows.Count;
+
+
+                    if (dataGridView1.AllowUserToAddRows == true)
+                    {
+                        rowCount = rowCount - 1;
+                    }
+
+                    for (int i = 0; i < rowCount; i++)
+                    {
+
+                        List<String> strList = new List<String>();
+
+
+                        for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                        {
+                            strList.Add(dataGridView1[j, i].Value.ToString());
+                        }
+                        String[] strArray = strList.ToArray();
+
+
+                        String strCsvData = String.Join(",", strArray);
+
+                        writer.WriteLine(strCsvData);
+                    }
+                }
+            }
+            else
+            { }
+        }
+        public void ExportFile()
+        {
+            SaveFileDialog sa = new SaveFileDialog();
+            sa.Title = "SAVE FILE";
+            sa.InitialDirectory = @"C:\";
+            sa.FileName = FileName;
+            sa.Filter = "CSV file(*.csv)|*.csv";
+            sa.FilterIndex = 1;
+
+            DialogResult result = sa.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+
+                string fileName = FileName;
+
+                using (StreamWriter writer = new StreamWriter(FileName))
+                {
+
+                    int rowCount = dataGridView1.Rows.Count;
+
+
+                    if (dataGridView1.AllowUserToAddRows == true)
+                    {
+                        rowCount = rowCount - 1;
+                    }
+
+                    for (int i = 0; i < rowCount; i++)
+                    {
+
+                        List<String> strList = new List<String>();
+
+
+                        for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                        {
+                            strList.Add(dataGridView1[j, i].Value.ToString());
+                        }
+                        String[] strArray = strList.ToArray();
+
+
+                        String strCsvData = String.Join(",", strArray);
+
+                        writer.WriteLine(strCsvData);
+                    }
+                }
+            }
+            else if (result == DialogResult.Cancel)
+            {
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
     }
 }
+
+    
